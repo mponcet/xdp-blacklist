b1d4537e5e7b9bdc03e37963583d8cb11b1fce3a
Maxime Poncet <maxime.poncet@gmail.com>
2020-05-01 19:17:41 +0200

    Implement IPv4 addr blacklist

--- xdp-blacklist.c
+++ xdp-blacklist.c
@@ -1,8 +1,69 @@
 #include <uapi/linux/bpf.h>
+#include <linux/kernel.h>
+#include <linux/in.h>
+#include <linux/if_ether.h>
+#include <linux/if_packet.h>
+#include <linux/ip.h>
 
 #define KBUILD_MODNAME "foo"
 
+struct ipv4_key {
+	u32 addr;
+};
+
+BPF_HASH(ipv4_blacklist, struct ipv4_key);
+
+static int ipv4_is_blacklisted(u32 addr)
+{
+	struct ipv4_key key = { addr };
+
+	if (ipv4_blacklist.lookup(&key))
+	    return 1;
+
+	return 0;
+}
+
+static struct ethhdr *ethhdr_ptr(struct xdp_md *ctx)
+{
+	struct ethhdr *ethhdr;
+
+	if (ctx->data + sizeof(*ethhdr) > ctx->data_end)
+		return NULL;
+
+	return (struct ethhdr *)(unsigned long)ctx->data;
+}
+
+static struct iphdr *iphdr_ptr(struct xdp_md *ctx, struct ethhdr *ethhdr)
+{
+	struct iphdr *iphdr;
+
+	if (ctx->data + sizeof(struct ethhdr) + sizeof(*iphdr) > ctx->data_end)
+		return NULL;
+
+	return (struct iphdr *)(unsigned long)(ctx->data + sizeof(struct ethhdr));
+}
+
+
 int xdp_main(struct xdp_md *ctx)
 {
+	struct ethhdr *ethhdr;
+	struct iphdr *iphdr;
+
+	ethhdr = ethhdr_ptr(ctx);
+	if (!ethhdr)
+		goto xdp_pass;
+
+	if (ethhdr->h_proto == bpf_htons(ETH_P_IP)) {
+		iphdr = iphdr_ptr(ctx, ethhdr);
+		if (!iphdr)
+			goto xdp_pass;
+
+		if (ipv4_is_blacklisted(iphdr->saddr)) {
+			bpf_trace_printk("ip in blacklist: DROP\n");
+			return XDP_DROP;
+		}
+	}
+
+xdp_pass:
 	return XDP_PASS;
 }
--- xdp-blacklist.py
+++ xdp-blacklist.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python
 
 import sys
+import ctypes as ct
 from bcc import BPF
+from netaddr import IPAddress
+from socket import htonl
+
+class KeyIPv4(ct.Structure):
+    _fields_ = [("addr", ct.c_uint32)]
 
 def usage():
     print("Usage: {0} <interface>".format(sys.argv[0]))
@@ -14,6 +20,11 @@ if __name__ == "__main__":
 
     device = sys.argv[1]
     bpf = BPF(src_file="xdp-blacklist.c")
+
+    ipv4_blacklist = bpf["ipv4_blacklist"]
+    google_dns = KeyIPv4(htonl(int(IPAddress("8.8.4.4"))))
+    ipv4_blacklist[google_dns] = ct.c_uint64(0)
+
     xdp_main = bpf.load_func("xdp_main", BPF.XDP)
     bpf.attach_xdp(device, xdp_main)
     input("Press enter to terminate...")
